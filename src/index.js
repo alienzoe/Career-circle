f7d11e62eb9c7dc58b08187e189de7a959f2ae73
Alienzoe <pro.jls04@gmail.com>
2018-08-05 00:35:22 +0800

    Add SCSS

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.min.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.min.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-import { BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ClientProvider, client } from "./client";
+
+import HomeComponent from "./components/HomeComponent";
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <React.Fragment>
         <Switch>
-          <Route exact path="/" component="Home">
-          <Route exact path="/login" component="Login">
-          <Route exact path="/signup" component="Signup">
+          <Route
+            exact
+            path="/"
+            /* component={HomeComponent} */ render={() => {
+              const root = document.querySelector("#root");
+              root.classList.add("home");
+
+              return <HomeComponent />;
+            }}
+          />
+          {/* <Route exact path="/login" component="Login" />
+          <Route exact path="/signup" component="Signup" /> */}
         </Switch>
-      </>
+      </React.Fragment>
     );
   }
 }
 
+ReactDOM.render(
+  <Router>
+    <ClientProvider client={client}>
+      <App />
+    </ClientProvider>
+  </Router>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
